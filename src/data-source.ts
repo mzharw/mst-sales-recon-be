169542977d41dff32cb99277c2242988f3e7436c
mzharw <mazhariwirasena@gmail.com>
2024-08-06 18:22:00 +0700

    fix: typeorm configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,21 +1,24 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
+const isProd = process.env.NODE_ENV === 'production';
 const dataSource = new DataSource({
   type: 'postgres',
+  url: process.env.DB_URL,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: process.env.NODE_ENV !== 'production',
+  synchronize: !isProd,
   entities: [__dirname + '/**/*.entity{.ts,.js}', __dirname + '/**/*.orm-entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   migrationsTableName: 'migration_table',
   dropSchema: false,
   logging: false,
   logger: 'file',
+  ssl: isProd,
 });
 
 export default dataSource;
